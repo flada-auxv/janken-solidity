e9c2e436a2ac6b526a84485776b80d5002d1207b
flada-auxv <aseknock@gmail.com>
2019-05-26 01:50:11 +0900

    createGame()

--- contracts/Janken.sol
+++ contracts/Janken.sol
@@ -4,9 +4,36 @@ contract Janken {
   enum Hand { Rock, Paper, Scissors }
   enum Result { Draw, Win, Loss }
 
+  uint public gameId = 0;
+  mapping (uint => Game) public games;
+  struct Game {
+    uint requiredDeposit;
+    address owner;
+    address opponent;
+    uint256 owner_encrypted_hand;
+    uint256 opponent_encrypted_hand;
+    uint256 owner_decrypted_hand;
+    uint256 opponent_decrypted_secret;
+    uint256 owner_secret;
+    uint256 opponent_secret;
+  }
+
   function startSolo(Hand hand) public pure returns (Result) {
     return judge(hand, Hand.Rock);
   }
+
+  function createGame() public payable {
+    gameId += 1;
+    Game storage game = games[gameId];
+    game.owner = msg.sender;
+    game.requiredDeposit = msg.value;
+  }
+
+  // function joinGame(gameId) {}
+
+  // function commitHand(gameId, hashedHand) {}
+
+  // function revealHand(gameId, secret, hand) {}
 
   function judge(Hand hand1, Hand hand2) public pure returns (Result) {
     if (hand1 == hand2) {
--- test/janken.js
+++ test/janken.js
@@ -12,7 +12,44 @@ const RESULT = {
   LOSS: 2,
 }
 
+let instance;
+
 contract('Janken', (accounts) => {
+  const deploy = async () => {
+    instance = await Janken.new();
+  };
+
+  describe('createGame', () => {
+    beforeEach(deploy);
+
+    context('with sending a few ETH', () => {
+      it('should create new game and save deposit amount', async () => {
+        await instance.createGame({from: accounts[0], value: 10});
+        assert.equal(1, await instance.gameId.call());
+
+        const firstGame = await instance.games.call(1);
+        assert.equal(accounts[0], firstGame.owner);
+        assert.equal(10, firstGame.requiredDeposit);
+
+        await instance.createGame({from: accounts[1], value: 42});
+        assert.equal(2, await instance.gameId.call());
+
+        const secondGame = await instance.games.call(2);
+        assert.equal(accounts[1], secondGame.owner);
+        assert.equal(42, secondGame.requiredDeposit);
+      });
+    });
+
+    context('without sending any ETH', () => {
+      it('should revert', async () => {
+        await instance.createGame();
+        const createdGame = await instance.games(1);
+        assert.equal(accounts[0], createdGame.owner);
+        assert.equal(0, createdGame.requiredDeposit); // FIXME!!
+      });
+    })
+  });
+
   describe('startSolo', () => {
     it('should return the code of draw', async () => {
       const instance = await Janken.deployed();
