98060c2d4dc52126a727fed95ccf6a4fa463816d
flada-auxv <aseknock@gmail.com>
2019-06-08 23:06:02 +0900

    use solidity-coverage

--- truffle-config.js
+++ truffle-config.js
@@ -47,6 +47,13 @@ module.exports = {
       port: 8545, // Standard Ethereum port (default: none)
       network_id: '*', // Any network (default: none)
     },
+    coverage: {
+      host: '127.0.0.1',
+      port: 8555,
+      network_id: '*',
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
