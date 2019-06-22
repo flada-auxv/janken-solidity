c5e01ef6d7d5e12f80151295764c415e1dd73743
flada-auxv <aseknock@gmail.com>
2019-06-22 22:00:35 +0900

    update sample ts file

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,1 +1,4 @@
-console.log('hi');
+const div: HTMLDivElement = document.createElement('div');
+const newContent: Text = document.createTextNode('hi!');
+div.appendChild(newContent);
+document.body.appendChild(div);
